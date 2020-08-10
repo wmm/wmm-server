c9d21a2265287cc50f331c9f2c378f0b54f443b1
Martin Domajnko <domajnko.martin@gmail.com>
2020-08-10 12:37:06 +0200

    Change formating

--- test/data.js
+++ test/data.js
@@ -1,90 +1,90 @@
 const alice = {
-  username: "alice",
-  name: "alice test",
-  email: "alice.test@gmail.com",
-  password: "Superpassword123",
+	username: 'alice',
+	name: 'alice test',
+	email: 'alice.test@gmail.com',
+	password: 'Superpassword123',
 };
 
 const bob = {
-  username: "bob123",
-  name: "bob test",
-  email: "bob.test@gmail.com",
-  password: "Superpassword123",
+	username: 'bob123',
+	name: 'bob test',
+	email: 'bob.test@gmail.com',
+	password: 'Superpassword123',
 };
 
 const charlie = {
-  username: "charlie",
-  name: "charlie test",
-  email: "charlie.test@gmail.com",
-  password: "Superpassword123",
+	username: 'charlie',
+	name: 'charlie test',
+	email: 'charlie.test@gmail.com',
+	password: 'Superpassword123',
 };
 
 const emptyUsername = {
-  username: "",
-  name: "testing bot",
-  email: "test.bot@gmail.com",
-  password: "botspassword123",
+	username: '',
+	name: 'testing bot',
+	email: 'test.bot@gmail.com',
+	password: 'botspassword123',
 };
 
 const emptyName = {
-  username: "testbot",
-  name: "",
-  email: "test.bot@gmail.com",
-  password: "botspassword123",
+	username: 'testbot',
+	name: '',
+	email: 'test.bot@gmail.com',
+	password: 'botspassword123',
 };
 
 const emptyEmail = {
-  username: "testbot",
-  name: "testing bot",
-  email: "",
-  password: "botspassword123",
+	username: 'testbot',
+	name: 'testing bot',
+	email: '',
+	password: 'botspassword123',
 };
 
 const emptyPassword = {
-  username: "testbot",
-  name: "testing bot",
-  email: "test.bot@gmail.com",
-  password: "",
+	username: 'testbot',
+	name: 'testing bot',
+	email: 'test.bot@gmail.com',
+	password: '',
 };
 
 const invalidUsername = {
-  username: "F!",
-  name: "testing bot",
-  email: "test.bot@gmail.com",
-  password: "botspassword123",
+	username: 'F!',
+	name: 'testing bot',
+	email: 'test.bot@gmail.com',
+	password: 'botspassword123',
 };
 
 const invalidName = {
-  username: "testbot",
-  name: "F!",
-  email: "test.bot@gmail.com",
-  password: "botspassword123",
+	username: 'testbot',
+	name: 'F!',
+	email: 'test.bot@gmail.com',
+	password: 'botspassword123',
 };
 
 const invalidEmail = {
-  username: "testbot",
-  name: "testing bot",
-  email: "F!",
-  password: "botspassword123",
+	username: 'testbot',
+	name: 'testing bot',
+	email: 'F!',
+	password: 'botspassword123',
 };
 
 const invalidPassword = {
-  username: "testbot",
-  name: "testing bot",
-  email: "test.bot@gmail.com",
-  password: "F!",
+	username: 'testbot',
+	name: 'testing bot',
+	email: 'test.bot@gmail.com',
+	password: 'F!',
 };
 
 module.exports = {
-  alice,
-  bob,
-  charlie,
-  emptyUsername,
-  emptyName,
-  emptyEmail,
-  emptyPassword,
-  invalidUsername,
-  invalidEmail,
-  invalidName,
-  invalidPassword,
+	alice,
+	bob,
+	charlie,
+	emptyUsername,
+	emptyName,
+	emptyEmail,
+	emptyPassword,
+	invalidUsername,
+	invalidEmail,
+	invalidName,
+	invalidPassword,
 };

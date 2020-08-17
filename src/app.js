13174d5734a93f8b886aaa68089810df724795f8
Jakob Kordež <jakobkordez1999@gmail.com>
2020-08-17 16:36:17 +0200

    Add readme and changes in app.js

--- src/app.js
+++ src/app.js
@@ -14,7 +14,11 @@ const app = express();
 // CORS
 app.use(cors());
 
-app.use(logger('dev'));
+// No logs while running tests
+if (process.env.NODE_ENV !== 'test') {
+    app.use(logger('dev'));
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
